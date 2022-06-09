2438bafae2d16c1599b5fdcf83a08a6970dbbfd6
oyeshidevictoria <oyeshidevictoria@gmail.com>
2022-06-09 23:52:50 +0100

    commit

--- state/models.py
+++ state/models.py
@@ -4,43 +4,35 @@ from django.db import models
 
 
 class People(models.Model):
-    name = models.CharField(max_length=500, null=True, blank=True)
-    nickname = models.CharField(max_length=500, null=True, blank=True)
+    name = models.CharField(max_length=500, blank=True)
+    nickname = models.CharField(max_length=500, blank=True)
+    address = models.ForeignKey("Address", on_delete=models.CASCADE)
 
     def _str_(self):
         return self.name
 
 
 class Address(models.Model):
-    people = models.OneToOneField(People, on_delete=models.CASCADE, null=True)
-    state = models.CharField(max_length=500, null=True, blank=True)
-    home_address = models.CharField(max_length=500, null=True, blank=True)
-    postalcode = models.CharField(max_length=500, null=True, blank=True)
+    state = models.CharField(max_length=500, blank=True)
+    home_address = models.CharField(max_length=500, blank=True)
+    postalcode = models.CharField(max_length=500, blank=True)
 
     def _str_(self):
         return self.state
 
 
 class Doctor (models.Model):
-    patients = models.ManyToManyField(People, through="Doctors")
-    people = models.ManyToManyField(People, on_delete=models.CASCADE,  null=True)
-    patient_name = models.CharField(max_length=500, null=True, blank=True)
-    people_id_number = models.CharField(max_length=500, null=True, blank=True)
-    number_of_patient = models.IntegerField(null=True, blank=True)
-
-
-class Doctors(models.Model):
-    patient = models.ForeignKey(Doctor, on_delete=models.CASCADE)
-    hospitals = models.ForeignKey(People, on_delete=models.CASCADE)
+    patient_name = models.CharField(max_length=500, blank=True)
+    people_id_number = models.CharField(max_length=500, blank=True)
+    number_of_patient = models.IntegerField(blank=True)
 
     def _str_(self):
-        return self.patient
+        return self.patient_name
 
 
 class Product(models.Model):
-    name = models.OneToOneField(People, on_delete=models.CASCADE, null=True)
-    product_name = models.CharField(max_length=500, null=True, blank=True)
-    product_category = models.CharField(max_length=500, null=True, blank=True)
+    product_name = models.CharField(max_length=500, blank=True)
+    product_category = models.CharField(max_length=500, blank=True)
     price = models. IntegerField(null=True, blank=True)
 
     def _str_(self):
@@ -48,9 +40,9 @@ class Product(models.Model):
 
 
 class Bio (models. Model):
-    people = models.OneToOneField(People, on_delete=models.CASCADE, null=True)
-    people_id = models.CharField(max_length=500, null=True, blank=True)
-    origin = models.CharField(max_length=500, null=True, blank=True)
+    people_id = models.CharField(max_length=500, blank=True)
+    origin = models.CharField(max_length=500, blank=True)
+    about = models.TextField(max_length=1000, blank=True)
 
-    def _str_(self):
-        return self.people_id
+    def __str__(self):
+        return self.origin
